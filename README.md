a2ea8f890bd1a1a5402e05304378fd75231b927f
Andrew <andrew1mail@gmail.com>
2024-05-15 23:36:00 +0800

    added readme and made files for specific funcitons

---
+++ README.md
@@ -0,0 +1,17 @@
+﻿# A Minesweeper Game Played on CLI
+
+Begin the game:
+![initialize game](/assets/init_game.png)
+
+Actions:
+![Actions](/assets/actions.png)
+
+Game Over:
+![Game Over](/assets/gameover.png)
+
+### How to run
+
+```
+npm i
+npm run start
+```
--- src/index.ts
+++ src/index.ts
@@ -1,139 +1,11 @@
 ﻿import { select, input } from "@inquirer/prompts";
+import { generateGrid } from "./initializeGame.js";
+import { cellReveal, showField } from "./utils.js";
 
-const directions = [
-  [0, 1],
-  [0, -1],
-  [1, 0],
-  [-1, 0],
-  [1, 1],
-  [1, -1],
-  [-1, 1],
-  [-1, -1],
-];
-
-const getValue = (cell: Cell) => {
-  if (cell.hasRevealed) {
-    if (cell.isMine) return "M";
-    // console.log("hello");
-    return `${cell.neighboringMines}`;
-  }
-  if (cell.hasFlag) return "F";
-  return "*";
-};
-
-const showField = async (gameGrid) =>
-  new Promise<boolean>((res, rej) => {
-    const [m, n] = [gameGrid.length, gameGrid[0].length];
-    let hasWon = true;
-    console.log(
-      [...Array(n).keys()]
-        .map((i) => {
-          if (i === 0) return "     " + (i + 1).toString().padEnd(2, " ");
-          return (i + 1).toString().padEnd(2, " ");
-        })
-        .join(" ")
-    );
-    gameGrid.forEach((row, i) =>
-      console.log(
-        `${(i + 1).toString().padStart(2, " ")} | ${row
-          .map((cell: Cell) => {
-            if (!cell.isMine) hasWon &&= cell.hasRevealed;
-            return getValue(cell);
-          })
-          .join(" ".padEnd(2, " "))}`
-      )
-    );
-    res(hasWon);
-  });
-
-const generateMines = (firstCellRow, firstCellCol, rows, cols, numMines) => {
-  const mines = new Set<number>();
-  while (mines.size <= numMines) {
-    let randomIdx = -1;
-    while (
-      mines.has(randomIdx) ||
-      randomIdx === firstCellRow * cols + firstCellCol
-    )
-      randomIdx = Math.floor(Math.random() * rows * cols);
-    mines.add(randomIdx);
-  }
-  return (idx) => mines.has(idx);
-};
-
-const countNeighborMines = (i, j, grid) => {
-  const [m, n] = [grid.length, grid[0].length];
-
-  if (grid[i][j].isMine) {
-    directions.forEach(([dx, dy]) => {
-      const [newI, newJ] = [i + dx, j + dy];
-      if (
-        newI >= 0 &&
-        newI < m &&
-        newJ >= 0 &&
-        newJ < n &&
-        !grid[newI][newJ].isMine
-      )
-        grid[newI][newJ].neighboringMines++;
-    });
-  }
-};
-
-const cellReveal = (i, j, grid: Cell[][]) => {
-  const [m, n] = [grid.length, grid[0].length];
-
-  grid[i][j].hasRevealed = true;
-  if (grid[i][j].neighboringMines === 0) {
-    directions.forEach(([dx, dy]) => {
-      const [newI, newJ] = [i + dx, j + dy];
-      if (
-        newI >= 0 &&
-        newI < m &&
-        newJ >= 0 &&
-        newJ < n &&
-        !grid[newI][newJ].hasRevealed &&
-        !grid[newI][newJ].isMine &&
-        !grid[newI][newJ].hasFlag
-      )
-        cellReveal(newI, newJ, grid);
-    });
-  }
-};
-
-const generateGrid = async (firstCellRow, firstCellCol, rows, cols, numMines) =>
-  new Promise<Cell[][]>((res, rej) => {
-    // generate mines
-    const isCellAMine = generateMines(
-      firstCellRow,
-      firstCellCol,
-      rows,
-      cols,
-      numMines
-    );
-    // generate map
-    const grid: Cell[][] = Array.from({ length: +rows }, (_, i) =>
-      Array.from({ length: +cols }, (_, j) => {
-        const cell: Cell = {
-          // value: "*",
-          hasFlag: false,
-          hasRevealed: false,
-          neighboringMines: 0,
-        };
-        if (isCellAMine(i * cols + j)) return { ...cell, isMine: true };
-        else return cell;
-      })
-    );
-    // count neighboring mines
-    grid.forEach((row, i) =>
-      row.forEach((_, j) => countNeighborMines(i, j, grid))
-    );
-
-    // reveal first cell
-    cellReveal(firstCellRow, firstCellCol, grid);
-    res(grid);
-  });
-
-const welcome = async () => {
+const initializeGame = async () => {
   console.log("Welcome to Minesweeper!");
+
+  // ask for input
   const rows = await input({
     message: "Enter the number of rows:",
     validate: (input) => +input > 0 && +input < 100,
@@ -147,7 +19,7 @@ const welcome = async () => {
     validate: (input) => +input > 0 && +input <= +rows * +cols - 1,
   });
 
-  // check too much rows cols mines?
+  // ask for the first cell
   const firstCellRow = await input({
     message: "Enter the row of the first cell (1-indexed):",
     validate: (input) => +input > 0 && +input <= +rows,
@@ -156,6 +28,8 @@ const welcome = async () => {
     message: "Enter the col of the first cell (1-indexed):",
     validate: (input) => +input > 0 && +input <= +cols,
   });
+
+  // generate the grid
   const grid = await generateGrid(
     +firstCellRow - 1,
     +firstCellCol - 1,
@@ -163,6 +37,8 @@ const welcome = async () => {
     cols,
     mines
   );
+
+  // show the field
   const hasWon = await showField(grid);
   if (hasWon) {
     console.log("Congratulations! You have won!");
@@ -213,13 +89,13 @@ const actions = async (grid: Cell[][]) => {
         console.log("Game Over! You stepped on a mine!");
         grid[+row - 1][+col - 1].hasRevealed = true;
         await showField(grid);
-        return;
+        process.exit();
       }
       cellReveal(+row - 1, +col - 1, grid);
       const allRevealed = await showField(grid);
       if (allRevealed) {
         console.log("Congratulations! You have won!");
-        return;
+        process.exit();
       }
       await actions(grid);
       break;
@@ -245,7 +121,7 @@ const actions = async (grid: Cell[][]) => {
       const allRevealed = await showField(grid);
       if (allRevealed) {
         console.log("Congratulations! You have won!");
-        return;
+        process.exit();
       }
       await actions(grid);
       break;
@@ -256,7 +132,7 @@ const actions = async (grid: Cell[][]) => {
 };
 
 const main = async () => {
-  const field = await welcome();
+  const field = await initializeGame();
   await actions(field);
 };
 
